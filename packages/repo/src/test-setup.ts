48057fbe40a7b7b73be5332bcd07bfcf6c9cd77c
Duane Johnson <duane@schoolai.com>
2025-11-29 16:09:29 -0700

    chore: remove unused json-lines-formatter for logging

--- packages/repo/src/test-setup.ts
+++ packages/repo/src/test-setup.ts
@@ -1,7 +1,6 @@
 import fs from "node:fs"
 import stream from "node:stream"
 import { configure, getConsoleSink, getStreamSink } from "@logtape/logtape"
-import { getJsonLinesFormatter } from "./utils/get-json-lines-formatter.js"
 
 const LOG_PIPE_PATH = "./log.jsonl"
 
@@ -10,12 +9,8 @@ const logPipeStream = fs.createWriteStream(LOG_PIPE_PATH, { flags: "w" })
 // Configure LogTape for tests
 await configure({
   sinks: {
-    console: getConsoleSink({
-      formatter: getJsonLinesFormatter({ properties: "flatten" }),
-    }),
-    file: getStreamSink(stream.Writable.toWeb(logPipeStream), {
-      formatter: getJsonLinesFormatter({ properties: "flatten" }),
-    }),
+    console: getConsoleSink(),
+    file: getStreamSink(stream.Writable.toWeb(logPipeStream)),
   },
   loggers: [
     {
@@ -32,9 +27,6 @@ await configure({
   ],
 
   filters: {
-    // program: record => {
-    //   return record.category.includes("synchronizer-program")
-    // },
     channel: record => {
       const message = record.message[0]
       if (!message || typeof message !== "string") return false
